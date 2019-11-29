c64fc05498737cd03fdf9d2aa0b07cfd93fe8645
Preston Hudson <prestonleehudson@gmail.com>
2019-11-29 15:13:16 -0600

    Wrote a module to cube a number. Used a method from statistics module. Imported cube module.

---
+++ cubed.py
@@ -0,0 +1,8 @@
+# Create a module named cubed with a function that takes a number as a parameter, and returns the number cubed.
+
+def number_cubed(x):
+    """
+    :param x : int
+    """
+    x = x ** 3
+    return x
