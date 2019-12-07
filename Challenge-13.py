ed58a49ba9d56c8be6b48a387e905d5b51206fab
Preston Hudson <prestonleehudson@gmail.com>
2019-12-07 15:54:17 -0600

    Addeed square class.

--- Challenge-13.py
+++ Challenge-13.py
@@ -1,1 +1,13 @@
 # Preston Hudson 12/6/19 Challenge 13 Written in Python
+
+#1. Add a class variable to a class called Square so that every time you create a new Square object.
+
+class Shape():
+    def __init__(self):
+        print("Iam a shape!")
+
+
+class Square(Shape):
+    square_list = []
+
+    
